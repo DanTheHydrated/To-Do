8a2e0feb1e80f9ef93f7bb9d7ecb95343c7adfe1
Dan W <daniel.winn@student.awesomeinc.org>
2022-10-21 04:02:04 +0000

    first forward motion all day

--- app/src/Input.js
+++ app/src/Input.js
@@ -1,35 +1,33 @@
 import React from "react";
 import {useState} from "react";
-import DelButton from "./DelButton";
+
 
 
 let newId = 1;
-let thisA = 1;
 let taskFirst = [];
 
 export default function TaskSet() {
     const [body, setBody] = useState('');
+    const [active, setActive] = useState(1);
     const [task, setTask] = useState(taskFirst);
+    // const Clicked = () => {
+    //     setActive(0);
+    // };
 
     function handleClick() {
         const where = 0; 
         const taskNext = [
             ...task.slice(0, where),
-            { id: newId++, body: body, active: thisA++ },
+            { id: newId++, body: body, active: 1 },
             ...task.slice(where)
         ];
         setTask(taskNext);
         setBody('');
-    }
-
-    console.log(task);
-
-    function Clicked() {
-        setTask();
+        setActive(1);
+        console.log(task);
     };
 
-    let showing = task.filter((e)=> e.active=1);
-
+    let showing = task.filter((e)=> e.active===1);
 
     return (
         <>
@@ -44,7 +42,7 @@ export default function TaskSet() {
             <h5>{showing.length} :Tasks left</h5>
             <ul>
                 {showing.map(e => (
-                    <li key={e.id}>{e.body} <button onClick={Clicked()} /> </li>
+                    <li key={e.id}>{e.body} <button onClick={() => e.active=0}>finish</button> </li>
                 ))}
             </ul>
         </>
